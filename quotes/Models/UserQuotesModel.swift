7ca53c1fe9ae6fc59da59684e7f9f55fc36d6e9a
wilsonwei123 <wilsonwei2023@gmail.com>
2025-04-16 14:48:03 +1200

    Added ability for users to create own quotes.

---
+++ quotes/Models/UserQuotesModel.swift
@@ -0,0 +1,8 @@
+//
+//  UserQuotesModel.swift
+//  quotes
+//
+//  Created by Wilson Wei on 16/04/2025.
+//
+
+import Foundation
---
+++ quotes/Views/UserQuotesView.swift
@@ -0,0 +1,18 @@
+//
+//  UserQuotesView.swift
+//  quotes
+//
+//  Created by Wilson Wei on 16/04/2025.
+//
+
+import SwiftUI
+
+struct UserQuotesView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    UserQuotesView()
+}
